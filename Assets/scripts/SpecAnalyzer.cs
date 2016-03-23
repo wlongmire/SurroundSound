e5b4ee6c9355c79e3dcece45b27032aaae55960a
Warren Longmire <warrenlongmire@gmail.com>
2016-03-23 01:17:03 -0400

    abstracted away beatdata.

--- Assets/scripts/SpecAnalyzer.cs
+++ Assets/scripts/SpecAnalyzer.cs
@@ -1,42 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class SpecAnalyzer : MonoBehaviour {
 
-	private int subdivs = 1024;
-	private float[] samples;
 	private GameObject[] cubes;
+	private BeatData bd;
 	public AudioSource audio_source;
 
-
-	float sumSamples(int idx, int div) {
-		int start = div * idx;
-		int end = start + div;
-		float sum = 0;
-
-		for (int i = start; i < end; i++) {
-			sum += samples [i];
-		}
-
-		return (sum / div);
-	}
-
-	// Use this for initialization
 	void Start () {
-		samples = new float[subdivs];
+		bd = new BeatData (audio_source, 1024);
 		cubes = GameObject.FindGameObjectsWithTag ("cube");
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		AudioListener.GetOutputData(samples, 0);
+		bd.updateSamples ();
 
-		string s = "";
 		for(int i=0; i<cubes.Length; i++){
-			Debug.Log (sumSamples (i, samples.Length / cubes.Length));
-
-			cubes [i].transform.localScale = new Vector3 (sumSamples(i, samples.Length/cubes.Length)*20, 1, 1);
-//			cubes [i].transform.localPosition = new Vector3 (samples[i]*10, cubes[i].transform.position.y, cubes[i].transform.position.z);
+			cubes [i].transform.localScale = new Vector3 (bd.getSampleAvg(i, cubes.Length)*20, 1, 1);
 		}
 	}
 }
